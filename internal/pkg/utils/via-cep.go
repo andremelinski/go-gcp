4a221c9e7ca4926f81546550bb3623b70fd094ab
andremelinski <andremelinski29@gmail.com>
2024-07-07 09:39:24 -0300

    go gcp project

---
+++ internal/pkg/utils/via-cep.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"time"
+)
+
+type ViaCepDTO struct {
+	Api string 
+	Cep string `json:"cep"` 
+	Logradouro string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro string `json:"bairro"`
+	Localidade string `json:"localidade"`
+	UF string `json:"uf"`
+	IBGE string `json:"ibge"`
+	Gia string `json:"gia"`
+	DDD string `json:"ddd"`
+	Siafi string `json:"siafi"`
+}
+
+type ICepInfoAPI interface{
+	GetCEPInfo(cep string) (*ViaCepDTO, error)
+}
+
+type CepInfo struct{}
+
+func NewCepInfo() *CepInfo{
+	return &CepInfo{}
+}
+
+
+func (c *CepInfo)GetCEPInfo(cep string) (*ViaCepDTO, error){
+	ctx := context.Background()
+	// TODO vai pra env
+	ctx, cancel := context.WithTimeout(ctx, 1000*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://viacep.com.br/ws/"+cep+"/json/", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer req.Body.Close()
+	res, err := io.ReadAll(req.Body)
+	
+	if err != nil {
+		return nil, err
+	}
+	data := &ViaCepDTO{}
+	err = json.Unmarshal(res, data)
+	if err != nil {
+		return nil, err
+	}
+
+	if data.Bairro == "" {
+		return nil, errors.New(string(res))
+	}
+
+	return data, nil
+}
